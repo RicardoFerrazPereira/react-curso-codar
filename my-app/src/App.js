f48ad9ad432c84121441114ce46c50a1101ca6f7
Ricardo Ferraz <sourvivens@gmail.com>
2022-06-13 23:24:14 -0300

    aula01

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Ricardo'
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
   return (
     <div className="App">
-      <h1>Curso React</h1>
+      <h2>Curso React</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
     </div>
   );
 }
